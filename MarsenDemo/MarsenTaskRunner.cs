de217232e2ae04c0821a24ecab979d77ca6454cd
marklin <marklin@nine-yi.com>
2019-08-31 18:49:39 +0800

    init MarsenDemo

---
+++ MarsenDemo/MarsenTaskRunner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ParallelProcessPractice.Core;
+
+namespace MarsenDemo
+{
+    public class MarsenTaskRunner : TaskRunnerBase
+    {
+        public override void Run(IEnumerable<MyTask> tasks)
+        {
+            foreach (var task in tasks)
+            {
+                task.DoStepN(1);
+                task.DoStepN(2);
+                task.DoStepN(3);
+            }
+        }
+    }
+}
---
+++ MarsenDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using ParallelProcessPractice.Core;
+
+namespace MarsenDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskRunnerBase run = new MarsenTaskRunner();
+            run.ExecuteTasks(100); ;
+        }
+    }
+}
